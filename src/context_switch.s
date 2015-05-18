fdfa131179abe4b19cc723dcfbbe9b070995864d
dpcheval <dpcheval@>
2015-05-18 18:17:48 -0400

    Context switch is partially working. After fixing all of the registers so that they are pristine, then we should be able to loop infinitely

--- src/context_switch.s
+++ src/context_switch.s
@@ -22,11 +22,11 @@ kerexit:
 
 	} task_descriptor_t;
 	*/
-	mov	fp, sp
+	/*mov	fp, sp
 	str	r0, [fp,#0] @ first element will hold the pointer to the TD
 	str	r1, [fp,#4] @ second element will hold the pointer to the request
 	add sp,sp, #8
-	
+	*/
 	
 	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 	@   SUPER MODE (r0-r12, r15, r13_svc, r14_svc, cpsr, sprs_svc)
@@ -34,9 +34,10 @@ kerexit:
 
 	@ Start our code
     @ 1. Push kernel registers onto stack
-	stmfd   sp!, {r4, r5, r6, r7, r8, r9, sl, fp, ip, lr}
-	@store the TD and request pointers on of the kernel stack.
-	stmfd sp!, {r0,r1}
+
+	stmfd   sp!, {r4, r5, r6, r7, r8, r9, sl, fp, ip, lr,r0,r1}
+
+	
 	@ 3 save spsr
 	ldr r1, [r0, #4]
 	msr spsr_c, r1
@@ -88,9 +89,14 @@ kerexit:
 	.size	kerexit, .-kerexit
 	.ident	"GCC: (GNU) 4.0.2"
 
+	.global	kerenter
+	.type	kerenter, %function
 kerenter:
+
 	@ Go into system state
 	msr cpsr_c, #0x1F
+
+
 
 	@store all of the user registers on the user stack
 	stmfd sp!, {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, sl, fp, ip}
@@ -104,19 +110,29 @@ kerenter:
 	@load the TD and request pointers off of the kernel stack.
 	ldmfd sp!, {r0,r1}
 	
+	
+
 	@save the lr as the pc of TD
 	str lr, [r0, #8] 
 	@save the sp, obtained above to the sp of TD
 	str r3, [r0, #0]
+
+
+
 	@load the spsr of the active task
 	mrs r3, spsr
 	@save the spsr to the spsr of TD
 	str r3, [r0, #4]
 
-	@save the request data
-	str r4, [r1,#0]
+
 	
+	@save the request data
+	str r4, [r1,#0] @this line is broken
+
 	@reload the kernel's register
 	ldmfd   sp!, {r4, r5, r6, r7, r8, r9, sl, fp, ip, lr}
-
+	
+	mov pc, lr
 	@fill in the request with its args
+	.size	kerenter, .-kerenter
+	.ident	"GCC: (GNU) 4.0.2"
