ce854289d09598c2ca25c717d059bce3a6920134
Benjamin Robert Konyi <brkonyi@uwaterloo.ca>
2015-05-19 10:26:19 -0400

    Changed include directory structure and updated Makefile to search these directories

---
+++ include/kernel/scheduler.h
@@ -0,0 +1,45 @@
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
+
+#include <ring_buffer.h>
+#include <task_handler.h>
+#include <global.h>
+
+/**
+ * @brief Initializes the scheduler data structures.
+ * @details Initializes the scheduler data structures.
+ */
+void init_scheduler(global_data_t* global_data);
+
+/**
+ * @brief Schedules a task to be run.
+ * @details Schedules a task to be run at a priority specified in the
+ *          priority field in task
+ * 
+ * @param task The task to be scheduled.
+ * @return -2 if priority in task is invalid, -1 if task is NULL, 0 on success
+ */
+int schedule(global_data_t* global_data, task_descriptor_t* task);
+
+/**
+ * @brief Determines the next task to be run.
+ * @details Determines the next task to be run. Reschedules the current active task.
+ * @return The new active task to be run.
+ */
+task_descriptor_t* schedule_next_task(global_data_t* global_data);
+
+/**
+ * @brief Returns the currently active task.
+ * @details Returns the currently active task.
+ * @return NULL if no task has been scheduled yet, the active task otherwise.
+ */
+task_descriptor_t* get_active_task(global_data_t* global_data);
+
+/**
+ * @brief Turns the current task into a zombie that won't be scheduled again.
+ * @details Turns the current task into a zombie that won't be scheduled again. Does not
+ *          release task descriptor to be reused.
+ */
+void zombify_active_task(global_data_t* global_data);
+
+#endif
---
+++ include/kernel/task_handler.h
@@ -0,0 +1,44 @@
+#ifndef __TASK_HANDLER_H__
+#define __TASK_HANDLER_H__
+
+#include <common.h>
+#include <global.h>
+
+/**
+ * @brief Initializes the task descriptor handler
+ * @details Initializes the task descriptor handler
+ */
+void init_task_handler(global_data_t* global_data);
+
+/**
+ * @brief instantiate a task
+ * @details Create allocates and initializes a task descriptor, using the
+ * given priority, and the given function pointer as a pointer to the entry
+ * point of executable code, essentially a function with no arguments and no
+ * return value. When create_task returns the task descriptor has all the state
+ * needed to run the task, the task’s stack has been suitably initialized, and
+ * the task has been entered into its ready queue so that it will run the next
+ * time it is scheduled.
+ * 
+ * @param priority [description]
+ * @param code [description]
+ * 
+ * @return
+ *  id – the positive integer task id of the newly created task. The task id 
+ *      must be unique, in the sense that no task has, will have or has had the
+ *      same task id.
+ *  -1 – if the priority is invalid.
+ *  -2 – if the kernel is out of task descriptors.
+ */
+int create_task(global_data_t* global_data, priority_t priority, void (*code)());
+
+/**
+ * @brief Get a task descriptor for a given tid.
+ * @details Get a task descriptor for a given tid.
+ * 
+ * @param tid The tid of the task to return.
+ * @return The task descriptor associated with tid.
+ */
+task_descriptor_t* get_task(global_data_t* global_data, tid_t tid);
+
+#endif //__TASK_HANDLER_H__
