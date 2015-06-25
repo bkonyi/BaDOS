4d9a147897c8757e653627536b0b2c4ac8bb261c
dpcheval <dpcheval@student.cs.uwaterloo.ca>
2015-06-25 09:50:53 -0400

    Added some more robustness to our train server. Used a much prettier way of having different message sizes

--- include/user/trains/train_server.h
+++ include/user/trains/train_server.h
@@ -10,7 +10,13 @@ typedef enum train_server_cmd_t {
 
 typedef struct train_server_msg_t {
 	train_server_cmd_t command;
+    uint32_t num1;
 } train_server_msg_t;
+
+typedef struct train_server_sensor_msg_t {
+	train_server_cmd_t command;
+    char sensors[10];
+} train_server_sensor_msg_t;
 
 /**
  * @brief A server that is used to act as the controller for a train.
