035d0df3fbd3623304d745bbc6abcc24355e5905
dpcheval <dpcheval@>
2015-07-25 02:53:19 -0400

    changed the granularity

--- src/user/trains/train_calibration_loader.c
+++ src/user/trains/train_calibration_loader.c
@@ -44,8 +44,8 @@ void load_calibration(int16_t train, train_position_info_t* train_position_info)
 }
 
 void _set_defaults(train_position_info_t* train_position_info, uint16_t* velocities) {
-    train_position_info->acceleration_thousandths_mm_ticks = 10;
-    train_position_info->decceleration_thousandths_mm_ticks =21;
+    train_position_info->acceleration_thousandths_mm_ticks = 100;
+    train_position_info->decceleration_thousandths_mm_ticks =210;
     int i;
     for(i = 0; i < MAX_STORED_SPEEDS; i ++) {
         train_position_info->default_av_velocity[i]=velocities[i];
@@ -121,8 +121,8 @@ void load_train_62_calibration_info(train_position_info_t* train_position_info) 
     train_position_info->short_move_time = train_62_short_move_time;
     
     _set_defaults(train_position_info,velocities);
-    train_position_info->acceleration_thousandths_mm_ticks = 10;
-    train_position_info->decceleration_thousandths_mm_ticks = 21;
+    train_position_info->acceleration_thousandths_mm_ticks = 100;
+    train_position_info->decceleration_thousandths_mm_ticks = 210;
     int i, j, k;
     for(i = 0; i < 80; ++i) {
         for(j = 0; j < MAX_AV_SENSORS_FROM; ++j) {
